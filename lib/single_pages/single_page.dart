36ac6e0817d027976c344f816c1a7af86954dfe9
dilbekdeveloper11 <dilbekbaxtiyorov23@gmail.com>
2022-04-10 16:55:50 +0500

    single pageda Widget ishlatildi

--- lib/single_pages/single_page.dart
+++ lib/single_pages/single_page.dart
@@ -141,13 +141,40 @@ class SinglePage extends StatelessWidget {
             ),
           ),
           SliverGrid(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 1,
-                mainAxisExtent: getH(150),
-              ),
-              delegate: SliverChildBuilderDelegate((context, index) {
-                return Widgets.Card(ntitle: "", height: height, width: width, colors: colors, opacity: opacity, smallTitle: smallTitle, smallTitleColor: smallTitleColor, ismi: ismi, amount: amount, lastInvoice: lastInvoice, number: number, date: date)
-              })),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              mainAxisExtent: getH(150),
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return Container(
+                  color: Colors.black,
+                  height: double.infinity,
+                  width: double.infinity,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: getW(22), top: getH(12)),
+                    child: Container(
+                      color: Colors.black,
+                      child: Widgets.Card(
+                        ntitle: "№ 154",
+                        height: 21,
+                        width: 49,
+                        colors: const Color(0xff49B7A5),
+                        opacity: 0.15,
+                        smallTitle: "Paid",
+                        smallTitleColor: const Color(0xff49B7A5),
+                        ismi: "Yoldoshova Ziyoda",
+                        amount: "1,200,000 UZS",
+                        lastInvoice: "№ 156",
+                        number: "6",
+                        date: "31.01.2021",
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
